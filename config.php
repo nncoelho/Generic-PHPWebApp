c0be321338feac569d51e82fc9e401e4630482e4
NNCoelho <coelho301181@gmail.com>
2022-09-06 07:12:49 +0100

    Composer & config update

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('APP_VERSION',       '1.0.0');
 
 // MYSQL
 define('MYSQL_SERVER',      'localhost');
-define('MYSQL_DATABASE',    'webdeveloper');
+define('MYSQL_DATABASE',    'genericphpwebapp');
 define('MYSQL_USER',        'admin');
-define('MYSQL_PASS',        'BeV6x21apAMO');
+define('MYSQL_PASS',        '');
 define('MYSQL_CHARSET',     'utf8');
